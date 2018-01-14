72e279756873c4935e1fe15c3ab712a50e71e7b9
CHOCO\choco <honnynoop@naver.com>
2018-01-14 21:40:05 +0900

    1-67 java 200

---
+++ src/kr/co/infopub/chapter/s011/Help.java
@@ -0,0 +1,14 @@
+package kr.co.infopub.chapter.s011;
+// 메서드
+public class Help {
+	// 메서드 선언
+	public static void showHelp() {
+		System.out.println("**********************************************");
+		System.out.println("*        이 프로그래밍은 Java200이 만들었습니다.      *");
+		System.out.println("**********************************************");
+	}
+	//메서드 호출
+	public static void main(String[] args) {
+		showHelp();        // '메서드이륾()'
+	}
+}
---
+++ src/kr/co/infopub/chapter/s018/FRUIT.java
@@ -0,0 +1,4 @@
+package kr.co.infopub.chapter.s018;
+public enum FRUIT{
+	APPLE, BANANA, MANGO
+}
